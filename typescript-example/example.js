6dd38b7466119e2695a7f33939c660b528643e8b
suheylaakgun <a.suheyladogan@gmail.com>
2024-01-14 03:02:24 +0300

    tsconfig.json örneği

--- typescript-example/example.js
+++
@@ -1,7 +0,0 @@
-let i = 0;
-i = "Text";
-
-console.log(i);
-
-//i değişkenine hem number hem string değeri
-// atanabiliyor; bu type safe olmamak demek...
--- typescript-example/index.js
+++ typescript-example/index.js
@@ -1,25 +1,25 @@
-var j; //: number => tip tanımıdır
+let j; //: number => tip tanımıdır
 j = 10.5;
 console.log(j);
 //j değişkeni number tipinde oldu
 //string değeri atanamaz; bu Type safe olmak demek...
 //fonksiyonda, tanımın bittiği, bloğun başladığı noktada tip tanımı yapılır.
-var myFunction = function () {
+const myFunction = () => {
     return 1;
 };
 function myFunction2() {
 }
-var function1 = myFunction();
-var function2 = myFunction2();
-var ProductService = /** @class */ (function () {
-    function ProductService() {
-    }
+let function1 = myFunction();
+let function2 = myFunction2();
+class ProductService {
     //any ; herhangi bir tip
-    ProductService.prototype.getAll = function () {
+    getAll() {
         return "Merhaba"; //1, true gibi farklı dönüş tiplerini de any kabul eder.
-    };
-    ProductService.prototype.getById = function (id) {
+    }
+    getById(id) {
         return { id: 1, name: "Telefon", price: 10 };
-    };
-    return ProductService;
-}());
+    }
+}
+let productService = new ProductService();
+let product = productService.getById(1);
+console.log(product.price);
